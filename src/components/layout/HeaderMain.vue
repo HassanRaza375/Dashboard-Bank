8155414e101042169439c0a115381c0768d20073
Syed Hassan Raza <hassanraza@satjapan.co>
2024-12-20 12:07:16 +0500

    updates

--- src/components/layout/HeaderMain.vue
+++ src/components/layout/HeaderMain.vue
@@ -1,24 +1,24 @@
 <script setup>
-import { useCommonStore } from '../../stores/common';
-const commonStore = useCommonStore();
-const toggleSidebar = () => {
-  commonStore.ShowHideSidebar();
-};
+import { defineProps, defineEmits } from 'vue';
+
+defineProps({
+    togglesidebar: Function,
+});
+
+const emit = defineEmits(['togglesidebar']);
 </script>
 <template>
     <div class="dashboard-header">
         <div class="items-header">
             <div class="flex justify-start align-center gap-2">
-                <img src="/src/assets/sidebar-icons/buger-menu.png" height="24" width="24" loading="lazy" alt="icon" @click="toggleSidebar">
+                <img src="/src/assets/sidebar-icons/buger-menu.png" height="24" width="24" loading="lazy" alt="icon"
+                    @click="$emit('togglesidebar')">
                 <h2>Overview</h2>
             </div>
             <div>
                 <!-- <input type="text"> -->
-
             </div>
         </div>
     </div>
-  </template>
-  <style>
-  
-  </style>
+</template>
+<style></style>
--- src/components/layout/sid.vue
+++ src/components/layout/sid.vue
@@ -1,59 +1,67 @@
 <script setup>
-import { ref } from 'vue';
+import { defineProps, defineEmits, ref } from 'vue';
 import { useCommonStore } from '../../stores/common';
 const commonStore = useCommonStore();
 
+defineProps({
+    togglesidebar: Function,
+});
+
+const emit = defineEmits(['togglesidebar']);
 const routesLinks = ref([
     {
-        imgsrc:'/src/assets/sidebar-icons/dashboard.svg',
-        linkName:'Dashboard',
-        linkUrl:'/'
+        imgsrc: '/src/assets/sidebar-icons/dashboard.svg',
+        linkName: 'Dashboard',
+        linkUrl: '/'
     },
     {
-        imgsrc:'/src/assets/sidebar-icons/transaction.svg',
-        linkName:'Transactions',
-        linkUrl:'/transaction'
+        imgsrc: '/src/assets/sidebar-icons/transaction.svg',
+        linkName: 'Transactions',
+        linkUrl: '/transaction'
     },
     {
-        imgsrc:'/src/assets/sidebar-icons/account.svg',
-        linkName:'Accounts',
-        linkUrl:'/account'
+        imgsrc: '/src/assets/sidebar-icons/account.svg',
+        linkName: 'Accounts',
+        linkUrl: '/account'
     },
     {
-        imgsrc:'/src/assets/sidebar-icons/investment.svg',
-        linkName:'Investments',
-        linkUrl:'/investments'
+        imgsrc: '/src/assets/sidebar-icons/investment.svg',
+        linkName: 'Investments',
+        linkUrl: '/investments'
     },
     {
-        imgsrc:'/src/assets/sidebar-icons/credit-card.svg',
-        linkName:'Credit Cards',
-        linkUrl:'/creditcard'
+        imgsrc: '/src/assets/sidebar-icons/credit-card.svg',
+        linkName: 'Credit Cards',
+        linkUrl: '/creditcard'
     },
     {
-        imgsrc:'/src/assets/sidebar-icons/loan.svg',
-        linkName:'Loan',
-        linkUrl:'/loan'
+        imgsrc: '/src/assets/sidebar-icons/loan.svg',
+        linkName: 'Loan',
+        linkUrl: '/loan'
     },
     {
-        imgsrc:'/src/assets/sidebar-icons/services.svg',
-        linkName:'Services',
-        linkUrl:'/services'
+        imgsrc: '/src/assets/sidebar-icons/services.svg',
+        linkName: 'Services',
+        linkUrl: '/services'
     },
     {
-        imgsrc:'/src/assets/sidebar-icons/privileges.svg',
-        linkName:'My Privileges',
-        linkUrl:'/myprivileges'
+        imgsrc: '/src/assets/sidebar-icons/privileges.svg',
+        linkName: 'My Privileges',
+        linkUrl: '/myprivileges'
     },
     {
-        imgsrc:'/src/assets/sidebar-icons/settings.svg',
-        linkName:'Setting',
-        linkUrl:'/setting'
+        imgsrc: '/src/assets/sidebar-icons/settings.svg',
+        linkName: 'Setting',
+        linkUrl: '/setting'
     }
 ])
 
 </script>
 <template>
     <div :class="commonStore.sidebar ? 'sidebar-item visible' : 'sidebar-item hidden'">
+        <div class="flex align-center justify-end">
+            <div class="close" @click="$emit('togglesidebar')"></div>
+        </div>
         <div class="logo-dashboard">
             <img src="/src/assets/dashboard-logo.png" width="183" height="36" alt="icon">
         </div>
@@ -62,7 +70,7 @@ const routesLinks = ref([
                 <li>
                     <img :src="link.imgsrc" height="25" width="25" loading="lazy" alt="icon">
                     <span>
-                      {{ link.linkName }}
+                        {{ link.linkName }}
                     </span>
                 </li>
             </router-link>
@@ -71,6 +79,4 @@ const routesLinks = ref([
 </template>
 
 
-<style>
-
-</style>
+<style></style>
